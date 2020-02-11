ab05948993cf05face596648cf8706d4b322006a
Yaser Jaradeh <Yaser.Jaradeh@tib.eu>
2020-02-11 18:05:44 +0100

    Fix random (y) index to choose the anchor column

--- random_selector.py
+++ random_selector.py
@@ -18,7 +18,7 @@ class RandomSelector:
     def choose_answer_randomly(self) -> str:
         values = self.df.values
         x = self.randomizer.randint(1, values.shape[0]-1)
-        y = self.randomizer.randint(1, values.shape[1]-1)
+        y = self.randomizer.randint(0, values.shape[1]-1)
         return values[x, y]
 
 
