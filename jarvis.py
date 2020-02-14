a98ec933ae3e63c05aa9f12698d878396d92a6ac
Yaser Jaradeh <Yaser.Jaradeh@tib.eu>
2020-02-14 16:16:16 +0100

    Add typing

--- jarvis.py
+++ jarvis.py
@@ -8,6 +8,6 @@ class JarvisQA:
         self.t2t = T2T()
         self.brain = QABrain(model, tokenizer)
 
-    def answer_question(self, csv_path, question, topk=3):
+    def answer_question(self, csv_path: str, question: str, topk: int = 3) -> list:
         context = self.t2t.table_2_text(csv_path)
         return self.brain.answer(question, context, top_k=topk)
