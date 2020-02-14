17d3b6ff6f8f9e13647748705a9c24ff55763c54
Yaser Jaradeh <Yaser.Jaradeh@tib.eu>
2020-02-14 16:17:18 +0100

    implement evaluate functions

--- evaluate.py
+++ evaluate.py
@@ -1,11 +1,12 @@
 import pandas as pd
-from baselines import BaseSelector, RandomSelector, LuceneSelector
+from baselines import RandomSelector, LuceneSelector
 from sklearn.metrics import precision_recall_fscore_support
+from jarvis import JarvisQA
 from time import time
 from memory_profiler import profile
 
 
-@profile
+#@profile
 def evaluate_random_baseline(dataset_path, top_k=3, qtype=None):
     df = pd.read_csv(dataset_path)
     y_true = []
@@ -15,7 +16,7 @@ def evaluate_random_baseline(dataset_path, top_k=3, qtype=None):
     for index, row in df.iterrows():
         question = row['Question']
         real_answer = row['Answer']
-        if qtype is not None and qtype in df.keys():
+        if qtype is not None:
             if row['Type'] != qtype:
                 continue
         y_true.append(real_answer if pd.notna(real_answer) else '')
@@ -32,7 +33,7 @@ def evaluate_random_baseline(dataset_path, top_k=3, qtype=None):
     return p, r, f1, pd.np.np.mean(times) * 1000
 
 
-@profile
+#@profile
 def evaluate_lucene_baseline(dataset_path, top_k=3, qtype=None):
     df = pd.read_csv(dataset_path)
     y_true = []
@@ -41,7 +42,7 @@ def evaluate_lucene_baseline(dataset_path, top_k=3, qtype=None):
     for index, row in df.iterrows():
         question = row['Question']
         real_answer = row['Answer']
-        if qtype is not None and qtype in df.keys():
+        if qtype is not None:
             if row['Type'] != qtype:
                 continue
         baseline = LuceneSelector(f'./datasets/orkg/csv/{row["Table"]}.csv')
@@ -60,12 +61,44 @@ def evaluate_lucene_baseline(dataset_path, top_k=3, qtype=None):
     return p, r, f1, pd.np.np.mean(times) * 1000
 
 
+#@profile
+def evaluate_jarvis(dataset_path, top_k=3, qtype=None, model_name='deepset/bert-large-uncased-whole-word-masking-squad2'):
+    df = pd.read_csv(dataset_path)
+    y_true = []
+    y_pred = []
+    times = []
+    qa = JarvisQA(model=model_name, tokenizer=model_name)
+    for index, row in df.iterrows():
+        question = row['Question']
+        real_answer = row['Answer']
+        if isinstance(real_answer, str):
+            real_answer = real_answer.strip('().{},\'"')
+        if qtype is not None:
+            if row['Type'] != qtype:
+                continue
+        y_true.append(real_answer if pd.notna(real_answer) else '')
+        start = time()
+        answers = qa.answer_question(f'./datasets/orkg/csv/{row["Table"]}.csv', question, top_k)
+        end = time()
+        times.append(end - start)
+        if real_answer in answers:
+            y_pred.append(real_answer)
+        else:
+            y_pred.append(answers[0] if len(answers) > 0 else '')
+    p, r, f1, _ = precision_recall_fscore_support(y_true, y_pred, average='macro', zero_division=0)
+    return p, r, f1, pd.np.np.mean(times) * 1000
+
+
 if __name__ == '__main__':
-    print(f'{"="*10} Random {"="*10}')
-    print(evaluate_random_baseline('./datasets/orkg/ORKG-QA-DS.csv', top_k=1))
-    print(evaluate_random_baseline('./datasets/orkg/ORKG-QA-DS.csv', top_k=2))
-    print(evaluate_random_baseline('./datasets/orkg/ORKG-QA-DS.csv', top_k=3))
-    print(f'{"=" * 10} Lucene {"=" * 10}')
-    print(evaluate_lucene_baseline('./datasets/orkg/ORKG-QA-DS.csv', top_k=1))
-    print(evaluate_lucene_baseline('./datasets/orkg/ORKG-QA-DS.csv', top_k=2))
-    print(evaluate_lucene_baseline('./datasets/orkg/ORKG-QA-DS.csv', top_k=3))
+    #print(f'{"="*10} Random {"="*10}')
+    #print(evaluate_random_baseline('./datasets/orkg/ORKG-QA-DS.csv', top_k=1))
+    #print(evaluate_random_baseline('./datasets/orkg/ORKG-QA-DS.csv', top_k=2))
+    #print(evaluate_random_baseline('./datasets/orkg/ORKG-QA-DS.csv', top_k=3))
+    #print(f'{"=" * 10} Lucene {"=" * 10}')
+    #print(evaluate_lucene_baseline('./datasets/orkg/ORKG-QA-DS.csv', top_k=1))
+    #print(evaluate_lucene_baseline('./datasets/orkg/ORKG-QA-DS.csv', top_k=2))
+    #print(evaluate_lucene_baseline('./datasets/orkg/ORKG-QA-DS.csv', top_k=3))
+    print(f'{"=" * 10} Jarvis base {"=" * 10}')
+    #print(evaluate_jarvis('./datasets/orkg/ORKG-QA-DS.csv', top_k=1))
+    #print(evaluate_jarvis('./datasets/orkg/ORKG-QA-DS.csv', top_k=2))
+    print(evaluate_jarvis('./datasets/orkg/ORKG-QA-DS.csv', top_k=5))
